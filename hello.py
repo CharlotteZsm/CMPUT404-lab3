82ff52082a5a1bc7af34229329422afbd69d9a34
CharlotteZsm <simiao4@ualberta.ca>
2022-09-28 22:30:58 -0600

    final

--- hello.py
+++ hello.py
@@ -13,6 +13,16 @@ for env_key, env_value in os.environ.items():
 
 print("Content-Type: application/json")    # HTML is following
 print()                             # blank line, end of headers
-
 # Print env dictionary in json format
 print(json.dumps(env))
+
+#https://www.pythonstudio.us/introduction-2/environment-variables.html
+#https://stackoverflow.com/questions/17665619/how-do-i-access-the-urls-query-string-in-a-python-cgi-script
+for param in os.environ.keys():
+    if (param=="QUERY_STRING"):
+        #print(f"<em>{param}</em> = {os.environ[param]}</li>")
+        print("<b>%20s</b>: %s<br>" % (param, os.environ[param]))
+
+for param in os.environ.keys():
+    if (param == "HTTP_USER_AGENT"):
+        print("<b>%20s</b>: %s<br>" % (param, os.environ[param]))
